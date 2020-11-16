c125ae1280af97424ac932711dc6f3cb35e125f4
Kailey Lampert <trepmal@gmail.com>
2020-11-16 10:53:19 -0800

    Use heredoc instead of output buffering to capture inline script

--- my-sites-search.php
+++ my-sites-search.php
@@ -91,24 +91,17 @@ function mss_enqueue_styles() {
  * @return void
  */
 function mss_enqueue_scripts() {
-	ob_start();
-	?>
+	$script = <<<SCRIPT
 jQuery(document).ready( function($) {
 	$('#wp-admin-bar-my-sites-search.hide-if-no-js').show();
 	$('#wp-admin-bar-my-sites-search input').keyup( function( ) {
-
 		var searchValRegex = new RegExp( $(this).val(), 'i');
-
 		$('#wp-admin-bar-my-sites-list > li.menupop').hide().filter(function() {
-
 			return searchValRegex.test( $(this).find('> a').text() );
-
 		}).show();
-
 	});
 });
-	<?php
-	$script = ob_get_clean();
+SCRIPT;
 
 	// the great jquery migrate compat
 	// see https://core.trac.wordpress.org/changeset/48323 for starters
